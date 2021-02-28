4ef37f854da45f8e266a898088afc753dda4f78a
Cristian Urbainski <cristian.urbainski@ciss.com.br>
2021-02-28 16:37:03 -0300

    Configurado object mapper para retornar apenas os campos não nulos no json.

--- src/main/java/br/com/urbainski/apipessoas/ApiPessoasApplication.java
+++ src/main/java/br/com/urbainski/apipessoas/ApiPessoasApplication.java
@@ -8,40 +8,56 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import br.com.urbainski.apipessoas.domain.PessoaSexo;
 
 @SpringBootApplication
 public class ApiPessoasApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(ApiPessoasApplication.class, args);
-	}
+        SpringApplication.run(ApiPessoasApplication.class, args);
+    }
 
-	@Bean
-	public ModelMapper getModelMapper() {
+    @Bean
+    public ObjectMapper getObjectMapper() {
 
-		ModelMapper mapper = new ModelMapper();
+        var objectMapper = new ObjectMapper();
 
-		mapper.addConverter(new Converter<String, PessoaSexo>() {
-			@Override
-			public PessoaSexo convert(MappingContext<String, PessoaSexo> mappingContext) {
+        objectMapper.setSerializationInclusion(Include.NON_NULL);
 
-				var source = mappingContext.getSource();
-				return StringUtils.isEmpty(source) ? null : PessoaSexo.fromCodigo(source);
-			}
-		});
+        objectMapper.registerModule(new JavaTimeModule());
 
-		mapper.addConverter(new Converter<PessoaSexo, String>() {
-			@Override
-			public String convert(MappingContext<PessoaSexo, String> mappingContext) {
+        return objectMapper;
+    }
 
-				var source = mappingContext.getSource();
-				return source == null ? null : source.getCodigo();
-			}
-		});
+    @Bean
+    public ModelMapper getModelMapper() {
 
-		return mapper;
-	}
+        var mapper = new ModelMapper();
+
+        mapper.addConverter(new Converter<String, PessoaSexo>() {
+            @Override
+            public PessoaSexo convert(MappingContext<String, PessoaSexo> mappingContext) {
+
+                var source = mappingContext.getSource();
+                return StringUtils.isEmpty(source) ? null : PessoaSexo.fromCodigo(source);
+            }
+        });
+
+        mapper.addConverter(new Converter<PessoaSexo, String>() {
+            @Override
+            public String convert(MappingContext<PessoaSexo, String> mappingContext) {
+
+                var source = mappingContext.getSource();
+                return source == null ? null : source.getCodigo();
+            }
+        });
+
+        return mapper;
+    }
 
 }
