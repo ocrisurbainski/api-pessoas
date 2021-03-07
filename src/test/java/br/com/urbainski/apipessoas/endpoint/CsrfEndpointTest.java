887369bbeb190322e4c914ec0b1bcc3e4c332c36
Cristian Urbainski <cristian.urbainski@ciss.com.br>
2021-03-07 20:54:18 -0300

    Adicionado teste unitário para o 'CsrfEndpoint'

---
+++ src/test/java/br/com/urbainski/apipessoas/endpoint/CsrfEndpointTest.java
@@ -0,0 +1,45 @@
+package br.com.urbainski.apipessoas.endpoint;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import br.com.urbainski.apipessoas.ApiPessoasApplication;
+
+/**
+ * @author cristian.urbainski
+ * @since 07/03/2021
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK, classes = ApiPessoasApplication.class)
+@AutoConfigureMockMvc
+public class CsrfEndpointTest {
+
+    private static final String UUID_REGEX = "[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetToken() throws Exception {
+
+        mockMvc.perform(get("/csrf").contentType(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("token", matchesPattern(UUID_REGEX)))
+                .andExpect(jsonPath("parameterName", is("_csrf")))
+                .andExpect(jsonPath("headerName", is("X-XSRF-TOKEN")));
+    }
+
+}
