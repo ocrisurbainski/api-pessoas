145ca629f4995fa42e26a0d753155468403255c4
Cristian Urbainski <cristian.urbainski@ciss.com.br>
2021-03-14 16:47:26 -0300

    Adicionado anotações do swagger ao endpoint source.

--- src/main/java/br/com/urbainski/apipessoas/endpoint/SourceEndpoint.java
+++ src/main/java/br/com/urbainski/apipessoas/endpoint/SourceEndpoint.java
@@ -3,15 +3,19 @@ package br.com.urbainski.apipessoas.endpoint;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.urbainski.apipessoas.config.SwaggerConfiguration;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * @author cristian.urbainski
@@ -24,6 +28,10 @@ public class SourceEndpoint extends AbstractEndpoint {
 
     @GetMapping
     @ApiOperation(value = "Devolve a url do repositório do projeto no GitHub.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Caso o projeto não esteja sendo executado")
+    })
     public ResponseEntity<Map<String, String>> getSource() {
 
         var map = new HashMap<String, String>();
